1e2c9cf87bcb85d85638025e234915dd7e85f5a5
Lukasz <lukaszkkoziel@gmail.com>
2020-07-13 13:02:42 +0900

    Create works

--- project1/blog/views.py
+++ project1/blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import (ListView,
+ DetailView,
+ CreateView)
 from .models import Post
 # Create your views here.
 
@@ -20,6 +22,17 @@ def about(request):
 
 class PostDetailView(DetailView):
   model = Post
+
+def about(request):
+  return render(request, 'blog/about.html', { 'title': 'About'})
+
+class PostCreateView(CreateView):
+  model = Post
+  fields = ['title', 'content']
+
+  def form_valid(self, form):
+    form.instance.author = self.request.user
+    return super().form_valid(form)
 
 def about(request):
   return render(request, 'blog/about.html', { 'title': 'About'})
